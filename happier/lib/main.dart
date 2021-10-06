42b5fdaf18b350409b5710ec2d09e9907f776f83
Pontus Backman <ponbac@student.chalmers.se>
2021-10-07 00:34:33 +0200

    trying cd with auth file

--- happier/lib/main.dart
+++ happier/lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:happier/blocs/current_view/current_view.dart';
 import 'package:happier/ui/app.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_dialogflow_v2/flutter_dialogflow_v2.dart' as Dialog;
 
 void main() {
+  initDialogFlow();
+
   runApp(MultiBlocProvider(providers: <BlocProvider>[
     BlocProvider<CurrentViewBloc>(
       create: (_) => CurrentViewBloc(),
@@ -13,28 +14,39 @@ void main() {
   ], child: const App()));
 }
 
-void initchatbot() async {
+Future<void> initDialogFlow() async {
+  // https://jsonkeeper.com/b/EHVY
+  DialogAuthCredentials credentials =
+      await DialogAuthCredentials.fromFile('assets/dialog_flow_auth.json');
 
-  print("[*] Retrieving key ..");
-  Dialog.AuthGoogle authGoogle =
-      await Dialog.AuthGoogle(fileJson: 'assets/happier-dialog_flow_key.json')
-          .build();
-  print("[*] Done");
-  /*Dialog.Dialogflow dialogflow =
-      Dialog.Dialogflow(authGoogle: authGoogle, sessionId: '123456');
-  Dialog.DetectIntentResponse response = await dialogflow.detectIntent(
-    Dialog.DetectIntentRequest(
-      queryInput: Dialog.QueryInput(
-        text: Dialog.TextInput(
-          text: 'Hello !',
-          languageCode: Dialog.Language.english,
-        ),
-      ),
-      queryParams: Dialog.QueryParameters(
-        resetContexts: true,
-      ),
-    ),
+  DialogFlowtter instance = DialogFlowtter(
+    credentials: credentials,
   );
-  */
-  //response.queryResult.queryText;
+
+  print('Initialized DialogFlow!');
 }
+
+// void initchatbot() async {
+//   print("[*] Retrieving key ..");
+//   Dialog.AuthGoogle authGoogle =
+//       await Dialog.AuthGoogle(fileJson: 'assets/happier-dialog_flow_key.json')
+//           .build();
+//   print("[*] Done");
+//   /*Dialog.Dialogflow dialogflow =
+//       Dialog.Dialogflow(authGoogle: authGoogle, sessionId: '123456');
+//   Dialog.DetectIntentResponse response = await dialogflow.detectIntent(
+//     Dialog.DetectIntentRequest(
+//       queryInput: Dialog.QueryInput(
+//         text: Dialog.TextInput(
+//           text: 'Hello !',
+//           languageCode: Dialog.Language.english,
+//         ),
+//       ),
+//       queryParams: Dialog.QueryParameters(
+//         resetContexts: true,
+//       ),
+//     ),
+//   );
+//   */
+//   //response.queryResult.queryText;
+// }
