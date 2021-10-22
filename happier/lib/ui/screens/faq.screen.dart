ca5f0f017b66b64726bdac84e269e19a14e13dd1
Pontus Backman <ponbac@student.chalmers.se>
2021-10-22 16:41:33 +0200

    cleaned up faw and help screens

--- happier/lib/ui/screens/faq.screen.dart
+++ happier/lib/ui/screens/faq.screen.dart
@@ -8,128 +8,86 @@ class FAQScreen extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(8),
       children: <Widget>[
-
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-          decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Color.fromRGBO(0, 83, 79, 1),
-                width: 2.0
-            )
-          ),
-          ),
-          child: const Text('Can I trust the findings of Hap?',
-            style: TextStyle(fontSize: 18)
-          )
-        ),
-
-        Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
+            ),
+            child: const Text('Can I trust the findings of Hap?',
+                style: TextStyle(fontSize: 18))),
+        Container(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('How do I contact Happier?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('How can Hap help me?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('How do I contact someone to help me?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('How much does it cost to use Happier?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('Can I use Happier in any country?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('When should I ask for help from a psychologist?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
+                style: TextStyle(fontSize: 18))),
         Container(
-            padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
-            decoration: BoxDecoration(
+            padding:
+                const EdgeInsets.only(top: 25.0, bottom: 25.0, left: 5.0, right: 5.0),
+            decoration: const BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: Color.fromRGBO(0, 83, 79, 1),
-                      width: 2.0
-                  )
-              ),
+                      color: Color.fromRGBO(0, 83, 79, 1), width: 2.0)),
             ),
             child: const Text('What data does Happier collect?',
-                style: TextStyle(fontSize: 18)
-            )
-        ),
-
-
+                style: TextStyle(fontSize: 18))),
       ],
     );
   }
--- happier/lib/ui/screens/help.screen.dart
+++ happier/lib/ui/screens/help.screen.dart
@@ -6,198 +6,181 @@ class HelpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Stack(children: <Widget>[
+        child: Stack(children: <Widget>[
       Column(
-      children: [
-      Container(padding:
-      const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-      margin: const EdgeInsets.only(left: 10, right: 10, bottom: 7, top: 14),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Colors.white, width: 2),
-          color: Colors.grey[100]),
-      child: Row(
         children: [
-          Expanded(
-            flex: 12,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Nathan Haudot',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF444444)),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top:15),
-                  child: const Text('Birth date: 2000-07-06')
-                ),
-                Container(
-                    padding: const EdgeInsets.only(top:2),
-                    child: const Text('Email: nathan.haudot@outlook.fr')
-                ),
-              ],
-            ),
-          ),
-          const Spacer(),
-
+          Container(
+              padding: const EdgeInsets.only(
+                  left: 14, right: 14, top: 10, bottom: 10),
+              margin: const EdgeInsets.only(
+                  left: 10, right: 10, bottom: 7, top: 14),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white, width: 2),
+                  color: Colors.grey[100]),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 12,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Nathan Haudot',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: const Text('Birth date: 2000-07-06')),
+                        Container(
+                            padding: const EdgeInsets.only(top: 2),
+                            child:
+                                const Text('Email: nathan.haudot@outlook.fr')),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                ],
+              )),
+          Container(
+              padding: const EdgeInsets.only(
+                  left: 14, right: 14, top: 10, bottom: 10),
+              margin:
+                  const EdgeInsets.only(left: 10, right: 10, bottom: 7, top: 7),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white, width: 2),
+                  color: Colors.grey[100]),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 12,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Name',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'First name'),
+                            )),
+                        const Text(
+                          'Last Name',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'Last name'),
+                            )),
+                        const Text(
+                          'Birth Date',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'YY/MM/DD'),
+                            )),
+                        const Text(
+                          'Address',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'Full address'),
+                            )),
+                        const Text(
+                          'Postal Code',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'XXX-XX'),
+                            )),
+                        const Text(
+                          'City',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'City name'),
+                            )),
+                        const Text(
+                          'Country',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF444444)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintText: 'Country name'),
+                            )),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                ],
+              )),
         ],
       )
-      ),
-        Container(padding:
-        const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 7, top: 7),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.white, width: 2),
-                color: Colors.grey[100]),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 12,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Name',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'Last Name',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'Birth Date',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'Address',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'Postal Code',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'City',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-                      const Text(
-                        'Country',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF444444)),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.only(top:15, bottom:15),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(12),
-                                hintText: 'Enter a search term'
-                            ),
-                          )
-                      ),
-
-                    ],
-                  ),
-                ),
-                const Spacer(),
-
-              ],
-            )
-        ),
-
-      ],
-    )])
-    );
+    ]));
   }
 }
